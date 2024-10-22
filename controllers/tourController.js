02804562ebcfc1569d7669accdd82d17643e2085
Your Name <urielparavi75@gmail.com>
2024-10-22 22:49:22 +0300

    branch pagination and alias-100

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
@@ -10,25 +17,46 @@ exports.getAllTours = async (req, res) => {
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
+    // Forex: difficulty=easy&name=The Forest Hiker
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
+    // Forex: Tour.find({ difficulty:'easy', duration: { $gte:5 } })
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
+      // Forex: -price -ratingsAverage
       query = query.sort(sortBy);
-      // query = query.sort(req.query.sort);
-      // console.log(req.query.sort);
-      // sort('price ratingsAverage')
     } else {
-      query = query.sort('-createdAt');
+      // query = query.sort('-createdAt');
+      query = query.sort('_id');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      // Forex: name,duration,difficulty,price => name duration difficulty price
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // Forex: page=3&limit=10 => 1-10, page 1, 11-20, page 2, 21-30, page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist.');
     }
 
     // EXECUTE QUERY
     const tours = await query;
+    // Forex: query.sort().select().skip().limit()
 
     //  const query = Tour.find()
     //  .where('duration')
